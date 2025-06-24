7c5e0d78c6d5d94ade9f17bb9b031701e3485360
Justin Zelikoff <justinzelikoff@Justins-Mac-mini.local>
2025-06-24 13:05:58 -0400

    commit three

--- testfile.py
+++ testfile.py
@@ -1,3 +1,6 @@
 print("this is a test file for git")
 
+print("git is nice but complicated")
+
 # I have added this comment as a second commit test using lazygit.
+# This is my third commit comment test.
